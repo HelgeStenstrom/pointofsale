fba9a90f24db19cb590556fb7b9e54a1805236ef
Helge Stenström <h.stenstrom@gmail.com>
2022-04-22 08:55:01 +0200

    Removed labels on tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,5 @@
+import Lib
 import Test.HUnit
-import Lib 
 
 import Fractions
 
@@ -9,19 +9,16 @@ main = do
   runTestTT tests
   return ()
 
-test1 = TestCase (assertEqual "besrivning" 3 3)
+test1 = TestCase (assertEqual "same integers are equal" 3 3)
+
 test2 = TestCase (assertEqual "besrivning" 3 Lib.threex)
+
 test3 = TestCase (assertEqual "Same integers" (Whole 3) (Whole 3))
 
 test4 = TestCase (assertEqual "string rep" "3/1" (show $ Whole 3))
 
-tests = TestList [
-    TestLabel "tst1" test1
-  , TestLabel "tst2" test2
-  , TestLabel "tst3" test3
-  , TestLabel "tst4" test4
-  ]
-
+tests = TestList [test1, test2, test3, test4]
 
 f1 = Whole 3
+
 f2 = Whole (-5)
