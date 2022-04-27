b4072f93818e86db393ac53c756149139695e3a7
Helge Stenström <h.stenstrom@gmail.com>
2022-04-27 13:41:54 +0200

    simplify works for 4/2

--- src/Fractions.hs
+++ src/Fractions.hs
@@ -13,7 +13,7 @@ instance Eq Fraction where
   Whole a == Whole b = a == b
   (Whole a) == (Frac n d) = a == n
   (Frac n d) == (Whole a) = n == a
-  (Frac a b) == (Frac c d) =  (a == c) && (b == d)
+  (Frac a b) == (Frac c d) = (a == c) && (b == d)
 
 denom (Whole _) = 1
 denom (Frac _ b) = b
@@ -22,9 +22,17 @@ numer (Frac a _) = a
 numer (Whole n) = n
 
 instance Num Fraction where
-  (+) = undefined 
-  (*) = undefined 
-  abs = undefined 
-  signum = undefined 
-  fromInteger  = undefined 
-  (-) = undefined 
+  (+) = undefined
+  (*) = undefined
+  abs = undefined
+  signum = undefined
+  fromInteger = undefined
+  (-) = undefined
+
+simplify :: Fraction -> Fraction
+simplify (Frac n d) = Frac nn dd
+  where
+    factor = gcd n d
+    nn = quot n factor
+    dd = quot d factor
+simplify (Whole n) = Whole n
