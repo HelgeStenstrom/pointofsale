52bc465a775a166085866506a180345117bb9f21
Helge Stenström <h.stenstrom@gmail.com>
2022-04-29 23:43:49 +0200

    Handles two BC and unknown product.

--- src/PointOfSale.hs
+++ src/PointOfSale.hs
@@ -5,5 +5,6 @@ fromPos = "From the PointOfSale.hs file"
 displayText :: String -> String
 displayText bc 
  | bc == "12345" = "$1.99"
- | otherwise  = "$12.00"
+ | bc == "45678" = "$12.00"
+ | otherwise  = "Product not found. Please try again"
 
--- test/PointOfSaleTest.hs
+++ test/PointOfSaleTest.hs
@@ -5,11 +5,20 @@ import Test.HUnit (Test(TestCase, TestList), assertEqual)
 
 -- Can we check what is being printed?
 --  Can we check IO ()?
-
 x = fromPos
 
-posTests = TestList [testOneItemFound, testAnotherItemFound]
+posTests =
+  TestList [testOneItemFound, testAnotherItemFound, testProductNotFound]
 
-testOneItemFound  = TestCase (assertEqual "price of barcode 12345" "$1.99" (displayText "12345")) 
-testAnotherItemFound  = TestCase (assertEqual "price of barcode 45678" "$12.00" (displayText "45678")) 
+testOneItemFound =
+  TestCase (assertEqual "price of barcode 12345" "$1.99" (displayText "12345"))
 
+testAnotherItemFound =
+  TestCase (assertEqual "price of barcode 45678" "$12.00" (displayText "45678"))
+
+testProductNotFound =
+  TestCase
+    (assertEqual
+       "Product not found"
+       "Product not found. Please try again"
+       (displayText "99999"))
