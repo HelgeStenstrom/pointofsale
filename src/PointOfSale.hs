5bc7853111bb2a2c99dc5a000e7b50fe61040588
Helge Stenström <h.stenstrom@gmail.com>
2022-05-04 20:52:27 +0200

    Tests green, main is useless

--- src/PointOfSale.hs
+++ src/PointOfSale.hs
@@ -1,10 +1,20 @@
 module PointOfSale where
 
+import Control.Monad (forever)
+
 fromPos = "From the PointOfSale.hs file"
 
 displayText :: String -> String
-displayText bc 
- | bc == "12345" = "$1.99"
- | bc == "45678" = "$12.00"
- | otherwise  = "Product not found. Please try again"
+displayText bc
+  | bc == "12345" = "$1.99"
+  | bc == "45678" = "$12.00"
+  | otherwise = "Product not found. Please try again"
 
+iteration = do
+  bc <- getLine
+  putStr $ displayText bc
+  return ()
+
+main :: IO ()
+main = forever $ do iteration
+-- Hur kan en funktion "komma ihåg"?
